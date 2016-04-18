3b49c5cb753c98ac5b63abff767f2af321d45172
GebiOldWang <Email840226514@qq.com>
2016-04-18 11:05:46 +0800

    update

--- Excenple/OW_Keyboard/ViewController.m
+++ Excenple/OW_Keyboard/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "OW_Keyboard.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UITextField * text = [[UITextField alloc] initWithFrame:CGRectMake(100, 100, 130, 40)];
+    text.borderStyle = UITextBorderStyleBezel;
+    [self.view addSubview:text];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    //  自定义键盘
+    OW_Keyboard *keyboard = [OW_Keyboard keyboard];
+    text.inputView = keyboard;
+    
+    keyboard.numberKeyboardDian = ^(NSString * String){
+        //        是否为空  只能有一个小数点
+        text.text = [text.text stringByAppendingString:[NSString stringWithFormat:@"%@",String]];
+    };
+    
+    keyboard.numberKeyboardBackspace = ^(){
+        if (text.text.length != 0) text.text = [text.text substringToIndex:text.text.length -1];
+    };
+    
+    keyboard.numberKeyboardInput = ^(NSInteger number){
+        text.text = [text.text stringByAppendingString:[NSString stringWithFormat:@"%ld",number]];
+    };
+    
+    keyboard.completeBlock = ^(){
+        [text resignFirstResponder];
+    };
+
 }
 
 - (void)didReceiveMemoryWarning {
